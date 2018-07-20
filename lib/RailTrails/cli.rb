5411a7a5d3c7fdca62205791474bd1eeea41e962
ValerieHMoore <periwinkle17@outlook.com>
2018-07-20 13:31:52 -0400

    fix list of railtrails

--- lib/RailTrails/cli.rb
+++ lib/RailTrails/cli.rb
@@ -16,8 +16,7 @@ attr_accessor :name
     puts "Type exit to exit the program"
     input = gets.strip
     if input =~ /\A[0-9]{5}\z/
-      list_railtrails
-      RailTrails::Scraper.new("https://www.traillink.com/trailsearch/?mmloc=" + input).scrape
+      list_railtrails(input)
       elsif input.downcase == "exit"
         puts "Happy Trails! Please consider making a donation to www.railstotrails.org"
         else
@@ -26,8 +25,12 @@ attr_accessor :name
         end
   end
   
-  def list_railtrails
+  def list_railtrails(input)
     puts "Here is a list of railtrails near you:"
+    RailTrails::Scraper.new("https://www.traillink.com/trailsearch/?mmloc=" + input).scrape
+    RailTrails::RailTrail.all.each.with_index(1) do |trail, index|
+      puts "#{index}. #{trail.name}"
+    end
   end
   
   def try_again
--- lib/RailTrails/scraper.rb
+++ lib/RailTrails/scraper.rb
@@ -4,17 +4,28 @@ class RailTrails::Scraper
   def initialize(homepage_url)
     self.homepage = homepage_url
   end
-  #need mo coffee
+  
   def scrape
     doc = Nokogiri::HTML(open(self.homepage))
-    doc.css("div.trails div.row").each do |trail|
-      name = trail.css("div.column.details")[0].css("span")[0].text
-      url = trail.css("div.column.details a").attr("href").value
-      length = trail.css("div.column.details")[0].css("span")[1].text
-      state = trail.css("div.column.details")[0].css("span")[3].text
-      surface = trail.css("div.column.details")[0].css("span")[4].text
-      RailTrails::RailTrail.create(name, url, length, state, surface)
-    end
+      doc.css("div.trails div.row").each do |trail|
+        name = trail.css("div.column.details")[0].css("span")[0].text
+        url = trail.css("div.column.details a").attr("href").value
+        length = trail.css("div.column.details")[0].css("span")[1].text
+        state = trail.css("div.column.details")[0].css("span")[3].text
+        surface = trail.css("div.column.details")[0].css("span")[4].text
+        RailTrails::RailTrail.create(name, url, length, state, surface)
+      end
   end
   
+def scrape_details
+  #railtrails.all.each do |trail|
+  #open the individual trail url
+  #scrape the info there
+  #store that stuff in the object
+  RailTrails::RailTrail.all each do |trail|
+    doc = Nokogiri::HTML(open(trail.url))
+    
+  end
+end
+
 end
